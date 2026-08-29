cabc2eb1a432f14495506b1c5d659052cf025665
Tim Yusupov <timboyus@gmail.com>
2026-08-29 16:05:00 +0300

    feat(routing): add react-router and the route skeleton

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import './styles/index.css';
@@ -8,8 +9,10 @@ import store from './store/store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
+import pluginRewriteAll from 'vite-plugin-rewrite-all';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react(), tsconfigPaths()],
+  plugins: [svgr(), react(), tsconfigPaths(), pluginRewriteAll()],
   resolve: {
     alias: {
       '~styles': path.resolve(__dirname, 'src/styles'),
